ea653bc929f3729d47fb9e051f78db0c732e25fe
Will Roden <will@roden.cc>
2023-05-22 19:13:55 -0500

    lint

--- cmd/root_test.go
+++ cmd/root_test.go
@@ -12,17 +12,17 @@ import (
 
 func TestRun(t *testing.T) {
 	dir := t.TempDir()
-	_, err := run(dir, internal.GenerateVersionOptions{PrereleasePrefix: "dev"})
+	_, err := run(dir, &internal.GenerateVersionOptions{PrereleasePrefix: "dev"})
 	require.Error(t, err)
 
 	repo, err := git.PlainInit(dir, false)
 	require.NoError(t, err)
-	_, err = run(dir, internal.GenerateVersionOptions{PrereleasePrefix: "dev"})
+	_, err = run(dir, &internal.GenerateVersionOptions{PrereleasePrefix: "dev"})
 	require.Error(t, err)
 
 	commit1 := testutil.CreateEmptyCommit(t, repo, "first", nil)
 	testutil.MustCreateTag(t, repo, "invalid", commit1)
-	got, err := run(dir, internal.GenerateVersionOptions{PrereleasePrefix: "dev"})
+	got, err := run(dir, &internal.GenerateVersionOptions{PrereleasePrefix: "dev"})
 	require.Error(t, err)
 	require.Nil(t, got)
 
@@ -30,7 +30,7 @@ func TestRun(t *testing.T) {
 	testutil.MustCreateTag(t, repo, "v1.0.0", commit2)
 
 	commit3 := testutil.CreateEmptyCommit(t, repo, "second", []plumbing.Hash{commit2})
-	result, err := run(dir, internal.GenerateVersionOptions{PrereleasePrefix: "dev"})
+	result, err := run(dir, &internal.GenerateVersionOptions{PrereleasePrefix: "dev"})
 	require.NoError(t, err)
 	require.Equal(t, "v1.0.1-dev.1.g"+commit3.String()[0:7], *result)
 }
